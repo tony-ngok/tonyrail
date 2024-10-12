1c43e6f367ecab51de4c49e25f1efde83dbb4e4a
tony-ngok <tonyn1999@outlook.com>
2024-10-12 14:29:44 -0500

    new学习中

--- app/controllers/apohealths_controller.rb
+++ app/controllers/apohealths_controller.rb
@@ -1,4 +1,23 @@
+# Controller：将每个http请求分发到对应的方法（增删改查操作）处理
+# https://sg552.gitbooks.io/happy_book_rails/content/crud.html
+
 class ApohealthsController < ApplicationController
+  # 增
+  def new # GET /apohealths/new（展现创建项目的页面）
+    @apohealth = Apohealth.new
+  end
+
+  def create # POST /apohealths?各种参数
+    @apohealth = Apohealth.new(post_params)
+  end
+
+  # 删
+
+
+  # 改
+
+
+  # 查
   def index # GET /apohealths（查询所有数据）
     @apohealths = Apohealth.all # 调用查询所有数据
   end
--- app/controllers/time_controller.rb
+++ app/controllers/time_controller.rb
@@ -1,7 +1,6 @@
 # Controller：将每个http请求分发到对应的方法处理
 
 class TimeController < ApplicationController
-  def new
-    
+  def new # 无数据库，留空
   end
 end
